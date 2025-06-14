56757848670acd971ad2fb23067cf8ef1fce7bbf
checkr <ugochiori@gmail.com>
2025-06-14 15:06:19 +0100

    front end completed

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import GeneratePasscode from "./components/GeneratePasscode"
+
+
+function App() {
+
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+  <GeneratePasscode />
+  
+    </div>
+  )
+}
+
+export default App
